519ee952ddb10cfd9fa4a695a883a45a70638543
oseji <jiade1233@gmail.com>
2023-01-19 08:32:55 +0100

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      height: {
+        100: "500px",
+      },
+      width: {
+        76: "300px",
+        70: "270px",
+        54: "212px",
+        34: "135px",
+        150: "620px",
+        120: "580px",
+        84: "345px",
+        45: "45%",
+        65: "250px",
+      },
+      margin: {
+        1.5: "6px",
+        26: "100px",
+      },
+    },
+  },
+  plugins: [],
+};
